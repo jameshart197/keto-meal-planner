522f0f3136650f97f65f249466cb7a60a44de194
james hart <jamesmurphy197197@gmail.com>
2023-06-09 20:12:08 +0000

    [FIX] Reorder models so Post can use Meals model

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+MEAL_TYPE = ((0, "Breakfast"), (1, "Lunch"), (2, "Dinner"))
+
+
+class Meal(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    meal_type = models.IntegerField(choices=MEAL_TYPE)
+    preptime_in_minutes = models.DecimalField(max_digits=3, decimal_places=1)
+    calories_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    protein_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    fat_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    carbs_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+
+
+def __str__(self):
+    return self.title
+
+
 PRIVACY = ((0, "Private"), (1, "Public"))
 
 
@@ -10,25 +29,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     public = models.IntegerField(choices=PRIVACY)
-    # day = the user should be presented with all 7 days of the week 
+    # monday_breakfast = somethingsomething
     # with 3 meal types for each day
-    # content = the user should be able to use drop down menus for each day 
+    # content = the user should be able to use drop down menus for each day
     # to choose their meals from the Meal database
-
-
-MEAL_TYPE = ((0, "Breakfast"), (1, "Lunch"), (2, "Dinner"))
-
-
-class Meal(models.Model):
-
-    title = models.CharField(max_length=200, unique=True)
-    mealtype = models.IntegerField(choices=MEAL_TYPE)
-    preptime_mins = models.DecimalField(max_digits=3, decimal_places=1)
-    calories_g = models.DecimalField(max_digits=4, decimal_places=1)
-    protein_g = models.DecimalField(max_digits=4, decimal_places=1)
-    fat_g = models.DecimalField(max_digits=4, decimal_places=1)
-    carbs = models.DecimalField(max_digits=4, decimal_places=1)
-
-
-def __str__(self):
-    return self.title
