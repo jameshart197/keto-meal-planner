15065ca315503d7d9dfd53c3f4c32df3fc916617
james hart <jamesmurphy197197@gmail.com>
2023-06-09 20:12:32 +0000

    [WIP] Create view for dropdown selection for meals

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Meal
 
-# Create your views here.
+
+def meal_list(request):
+    breakfast_meals = Meal.objects.filter(meal_type=0)
+    lunch_meals = Meal.objects.filter(meal_type=1)
+    dinner_meals = Meal.objects.filter(meal_type=2)
+
+    context = {
+        'breakfast_meals': breakfast_meals,
+        'lunch_meals': lunch_meals,
+        'dinner_meals': dinner_meals,
+              }
+    return render(request, 'meal_list.html', context)
