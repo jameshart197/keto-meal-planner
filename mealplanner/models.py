5c0eb234480e096d9b4813a662448c6543b43031
james hart <jamesmurphy197197@gmail.com>
2023-06-09 15:09:34 +0000

    Add model for meal database

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+MEAL_TYPE = ((0, "Breakfast"), (1, "Lunch"), (2, "Dinner"))
+
+
+class Meal(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    mealtype = models.IntegerField(choices=MEAL_TYPE)
+    preptime_mins = models.DecimalField(max_digits=3, decimal_places=1)
+    calories_g = models.DecimalField(max_digits=4, decimal_places=1)
+    protein_g = models.DecimalField(max_digits=4, decimal_places=1)
+    fat_g = models.DecimalField(max_digits=4, decimal_places=1)
+    carbs = models.DecimalField(max_digits=4, decimal_places=1)
