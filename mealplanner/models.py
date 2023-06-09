e064b78698d513428cb6d3e9ad7fc52ce9109ca7
james hart <jamesmurphy197197@gmail.com>
2023-06-09 15:27:51 +0000

    [WIP] work on Post model

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+PRIVACY = ((0, "Private"), (1, "Public"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    public = models.IntegerField(choices=PRIVACY)
+    # day = the user should be presented with all 7 days of the week 
+    # with 3 meal types for each day
+    # content = the user should be able to use drop down menus for each day 
+    # to choose their meals from the Meal database
 
 
 MEAL_TYPE = ((0, "Breakfast"), (1, "Lunch"), (2, "Dinner"))
