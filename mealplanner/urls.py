6e46ecde4f8cd1278491969b4ccf06b8e5470c58
james hart <jamesmurphy197197@gmail.com>
2023-07-12 21:30:28 +0000

    [WIP] Add functionality for admins to edit and delete currently existing meals

--- mealplanner/urls.py
+++ mealplanner/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.meal_list, name='meal_list'),
     path('home/', views.home, name='home'),
     path('adminpanel/', views.adminpanel, name='adminpanel'),
+    path('adminpanel/add', views.add_adminpanel, name='add_adminpanel'),
+    path('adminpanel/edit', views.edit_adminpanel, name='edit_adminpanel'),
+    path('adminpanel/delete', views.delete_adminpanel, name='delete_adminpanel'),
 ]
--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,48 +1,99 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Meal, MealPlan
 from django.contrib import messages
 from .forms import MealForm
-
+from django.contrib.auth.decorators import login_required
 
 def meal_list(request):
-    
     return render(request, "index.html")
 
 
 def home(request):
     return render(request, "home.html")
 
-
+@login_required
 def adminpanel(request):
+    if not request.user.is_staff:
+        return redirect(
+            reverse('home')
+        )
+    meals = Meal.objects.all()
+    context = {
+        'meals': meals
+    }
+    return render(request, "adminpanel.html", context)
+
+
+@login_required
+def add_adminpanel(request):
+    if not request.user.is_staff:
+        return redirect(
+            reverse('home')
+        )
     if request.method == "POST":
-        form = MealForm(data=request.POST)
-        print(form.is_valid(), form.errors())
+        form = MealForm(request.POST)
         if form.is_valid():
             meal = form.save(commit=False)
+            mealplan = MealPlan.objects.all().first()
+            meal.mealplans = mealplan
             meal.save()
-            return redirect(
-                reverse('home')
-            )
-        else:
-            form = MealForm()
-            context = {
-                "form": form
-            }
-            return render(request, "adminpanel.html", context)
-    if request.method == "GET":
-        if not request.user.is_staff:
-            return redirect(
-                reverse('home')
-            )
-        form = MealForm()
-        context = {
-            "form": form
-        }
-        return render(request, "adminpanel.html", context)
+            return redirect(reverse('home'))
+    form = MealForm()
+    context = {
+        "form": form
+    }
+    return render(request, "add_adminpanel.html", context)
 
 
-# messages.error(request, "ERROR")
-    """
+@login_required
+def edit_adminpanel(request, pk):
+    if not request.user.is_staff:
+        return redirect(
+            reverse('home')
+        )
+
+    queryset = Meal.objects.all()
+    meal = get_object_or_404(queryset, pk=pk)
+
+    if request.method == "POST":
+        queryset = Meal.objects.all()
+        meal = get_object_or_404(queryset, pk=pk)
+        form = MealForm(request.POST, instance=meal)
+        if form.is_valid():
+            form.save()
+
+    form = MealForm(instance=meal)
+    context = {
+        "form": form
+    }
+    return render(request, "edit_adminpanel.html", context)
+
+
+@login_required
+def delete_adminpanel(request, pk):
+    if not request.user.is_staff:
+        return redirect(
+            reverse('home')
+        )
+    if request.method == "POST":
+        queryset = Meal.objects.all()
+        meal = get_object_or_404(queryset, pk=pk)
+        meal.delete()
+        return redirect(reverse('home'))
+
+    context = {
+        "pk": pk
+    }
+    return render(request, "delete_adminpanel.html", context)
+
+
+"""
+"""
+
+
+"""
+messages.error(request, "ERROR")
+
     if request.method == 'POST':
         breakfast_id = request.POST.get('breakfast')
         lunch_id = request.POST.get('lunch')
@@ -57,4 +108,4 @@ def adminpanel(request):
         'lunch_meals': lunch_meals,
         'dinner_meals': dinner_meals,
               }
-    """
+"""
